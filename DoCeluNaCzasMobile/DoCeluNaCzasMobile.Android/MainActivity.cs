93341a6735bf37b48fa8cbfa1bfa69041c776a21
s14222 <s14222@pjwstk.edu.pl>
2019-08-27 14:24:24 +0200

    app style

--- DoCeluNaCzasMobile/DoCeluNaCzasMobile.Android/MainActivity.cs
+++ DoCeluNaCzasMobile/DoCeluNaCzasMobile.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace DoCeluNaCzasMobile.Droid
 {
-    [Activity(Label = "DoCeluNaCzasMobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Do Celu Na Czas", Icon = "@drawable/iconDCNCZ", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
